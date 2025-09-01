b19dde21e40eb2a5412e3d7ac703fb50255b368d
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-08-31 21:44:49 -0600

    creating the models for the users table

--- LibraryUsers/models.py
+++ LibraryUsers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class LibraryUsers(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    password = models.IntegerField()
