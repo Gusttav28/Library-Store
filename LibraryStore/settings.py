1bffea9db1f8e058ebae2b80d4c3e5077af10821
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-08-31 21:25:47 -0600

    creating the models for the admin users site

--- LibraryStore/settings.py
+++ LibraryStore/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'LibraryStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LibraryStore',
+        'USER': 'root',
+        'PASSWORD': '30696222',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
