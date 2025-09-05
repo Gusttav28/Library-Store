ab4e4b07bb8b986dbf82e5d5d10831f11b1a633c
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-09-04 20:44:09 -0600

    adding the respectivites models to the Library book store app

--- Library_BookStore/models.py
+++ Library_BookStore/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class BooksAutors_Names(models.Model):
+    name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class BooksNames(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(BooksAutors_Names, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class BooksNew_Orders(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.ForeignKey(BooksAutors_Names, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
