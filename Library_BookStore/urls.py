66a80863ee0ed861b60f1d0b3580c4ff34aaaa13
Gustavo Camacho <Gusttav28@GMAIL.COM>
2025-10-05 20:42:50 -0600

    finished this issue

--- Library_BookStore/urls.py
+++ Library_BookStore/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView, name='home'),
-    path('BookStore/', views.BookStore, name='bookstore')
+    path('BookStore/', views.BookStore, name='bookstore'),
+    path('AdminSite/', views.AdminSite, name='admin')
 ]
